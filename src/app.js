14f8854647fc6b4ade1d5b8f57950db384ff3b62
Shaurya Awasthi <167126498+Shaurya130@users.noreply.github.com>
2026-02-08 03:38:01 +0530

    Refactor CORS configuration and reformat code

--- src/app.js
+++ src/app.js
@@ -1,36 +1,48 @@
-import express from "express"
-import cors from "cors" //allows wh can make a request to the server
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
-const app= express();
+const app = express();
 
-// middleware - in between configuration to do a certain task over the code
 
- app.set("trust proxy", 1);
+app.set("trust proxy", 1);
 
 app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN || "http://localhost:5173", // or your React app's URL
-        credentials: true
-    })
-)
-
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
-// routes
-
-import userRoutes from "./routes/user.routes.js"
-import postRoutes from "./routes/post.routes.js"
-
-app.use("/api/v1/users", userRoutes)
-app.use("/api/v1/posts", postRoutes)    
+  cors({
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        origin === "http://localhost:5173" ||
+        origin.endsWith(".vercel.app")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
 
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use(errorHandler)
 
-export { app }
+app.use(express.static("public"));
+
+
+app.use(cookieParser());
+
+
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
+
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
+
+
+app.use(errorHandler);
+
+export { app };
